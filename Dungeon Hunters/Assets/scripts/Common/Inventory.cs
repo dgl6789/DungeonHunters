94ecb0607e5c50042ef518c1d02357558c2f915c
dgl6789 <dgl6789@g.rit.edu>
2018-11-13 21:04:22 -0500

    some baseline inventory stuff

---
+++ Dungeon Hunters/Assets/scripts/Common/Inventory.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace App {
+    public class Inventory {
+
+        List<Item> items;
+        public Item this[Item item] {
+            get {
+                return items[items.IndexOf(item)];
+            }
+        }
+
+        public bool AddItem(Item item) {
+            items.Add(item);
+
+            return true;
+        }
+    }
+}
